911ec3fb8cf194386dca32d9bf76a2c633c71696
JordanSennett <jordansennett@yahoo.com>
2023-10-21 00:05:17 -0500

    34 s3 static website hosting (#37)
    * #34 enabled static website hosting, uploaded index and error file via terraform
    
    * #34 fixed some code

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -30,19 +30,32 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   
   #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object
 
-resource "aws_s3_bucket_object" "index_html" {
+ 34-s3-static-website-hosting
+resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
   source = var.index_html_filepath
+
+34-s3-static-website-hosting
+   etag = filemd5(var.index_html_filepath)
+}
+
+
+resource "aws_s3_object" "error_html" {
 
    etag = filemd5 (var.index_html_filepath)
 }
 
 
-resource "aws_s3_bucket_object" "error_html" {
+resource "aws_s3_object" "error_html" {
+
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
   source = var.error_html_filepath
 
    etag = filemd5(var.error_html_filepath)
 }
+ 34-s3-static-website-hosting
+ 
+
+
