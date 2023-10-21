d9118f313a320ee17a67ea08d766971018f2e6bd
JordanSennett <jordansennett@yahoo.com>
2023-10-21 01:40:37 -0500

    #36 create cdn and serve s3 bucket from cdn (#38)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -1,38 +1,41 @@
 variable "user_uuid" {
-  description = "User UUID"
+  description = "The UUID of the user"
   type        = string
   validation {
-    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.user_uuid))
-    error_message = "Invalid user UUID format. It should be in the form of a UUID (e.g., 123e4567-e89b-12d3-a456-426655440000)."
+    condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message    = "The user_uuid value is not a valid UUID."
   }
 }
 
 variable "bucket_name" {
-  description = "The name of the AWS S3 bucket"
+  description = "The name of the S3 bucket"
   type        = string
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9.-]{3,63}$", var.bucket_name))
-    error_message = "The bucket name must be between 3 and 63 characters long and can only contain letters, numbers, hyphens, and periods."
+    condition     = (
+      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
+      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
+    )
+    error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
   }
 }
 
 variable "index_html_filepath" {
-  description = "Path to the index.html file"
+  description = "The file path for index.html"
   type        = string
 
   validation {
-    condition = fileexists(var.index_html_filepath)
-    error_message = "The specified index.html file path does not exist."
+    condition     = fileexists(var.index_html_filepath)
+    error_message = "The provided path for index.html does not exist."
   }
 }
 
 variable "error_html_filepath" {
-  description = "Path to the error.html file"
+  description = "The file path for error.html"
   type        = string
 
   validation {
-    condition = fileexists(var.error_html_filepath)
-    error_message = "The specified error.html file path does not exist."
+    condition     = fileexists(var.error_html_filepath)
+    error_message = "The provided path for error.html does not exist."
   }
 }
